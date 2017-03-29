252b521be3a423a9ffce13ad9b07b8908e8102c1
Stas Pozdeev <stas.pazdzeev@gmail.com>
2017-03-29 18:19:56 +0300

    Create server, added upload images

---
+++ WebEWT/WebEWT/Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using WebEWT.Models;
+
+namespace WebEWT.Controllers
+{
+    public class ImageController : Controller
+    {
+        public ActionResult UploadImage()
+        {
+            return View();
+        }
+
+        public ActionResult AddImage(HttpPostedFileBase file)
+        {
+            var fileName = file.FileName;
+            var path = getPathToImg(fileName);
+            file.SaveAs(path);
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string getPathToImg(string fileName)
+        {
+            var serverPath = Server.MapPath("~");
+            return Path.Combine(serverPath, "Content", "img", fileName);
+        }
+
+        public JsonResult GetImages()
+        {
+            var serverPath = Server.MapPath("~");
+            var pathToImageFolder = Path.Combine(serverPath, "Content", "img");
+            var imageFiles = Directory.GetFiles(pathToImageFolder);
+            var imges = imageFiles.Select(buildImage);
+            return Json(imges, JsonRequestBehavior.AllowGet);
+        }
+
+        private Image buildImage(string path)
+        {
+            var fileName = Path.GetFileName(path);
+            var image = new Image
+            {
+                Url = Url.Content("~/Content/img/" + fileName)
+            };
+
+            return image;
+        }
+    }
+}
---
+++ WebEWT/WebEWT/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebEWT.Models
+{
+    public class Image
+    {
+        public string Url { get; set; }
+    }
+}
